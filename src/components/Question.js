d2d72ef1a53e213c0bf5b2fea86484100b64610d
Jonathas Artese <jonathas.artese@gmail.com>
2019-08-02 02:15:21 -0400

    Adding Questions component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { formatQuestion, formatDate } from '../utils/helpers'
+import { formatQuestion } from '../utils/helpers'
 /*import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline'
 import TiHeartOutline from 'react-icons/lib/ti/heart-outline'
 import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
@@ -8,6 +8,26 @@ import { handleToggleTweet } from '../actions/tweets'
 import { Link, withRouter } from 'react-router-dom'
 */
 class Question extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      choose: '',
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      choose: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    alert('Escolhido: ' + this.state.choose);
+    e.preventDefault();
+  }
   render() {
     const { question } = this.props
     const {
@@ -21,6 +41,29 @@ class Question extends Component {
                 alt={`Avatar of ${name}`}
                 className='avatar'
             />
+            <div className='tweet-indo'>
+              <span>{name}</span>
+              <div>Would you rather...</div>
+              <div>
+                <p>
+                  <input 
+                    type='radio' 
+                    value={optionOne.text} 
+                    checked={this.state.choose === optionOne.text} 
+                    onChange={this.handleInputChange}/>
+                  {optionOne.text}
+                </p>
+                <p>
+                  <input 
+                    type='radio' 
+                    value={optionTwo.text}  
+                    checked={this.state.choose === optionTwo.text}
+                    onChange={this.handleInputChange}/>
+                  {optionTwo.text}
+                </p>
+              </div>
+              <button type='submit' onClick={this.handleSubmit}>VOTE</button>
+            </div>
         </div>
     )
   }
